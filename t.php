2a7935a9213802026b6919c54aac169bb9b02e23
lucas pasnon <lucas.pasnon@outlook.fr>
2023-09-21 18:24:36 +0200

    Create t.php

---
+++ t.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page PHP avec Variable</title>
+</head>
+<body>
+    <?php
+    // Déclaration et initialisation de la variable
+    $nom = "John Doe";
+    ?>
+
+    <h1>Bienvenue sur ma page PHP</h1>
+    
+    <p>Bonjour, <?php echo $nom; ?> !</p>
+</body>
+</html>
