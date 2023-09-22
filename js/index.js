6ab923333fa43d12b3810053221cded04ff15db2
lucas pasnon <lucas.pasnon@outlook.fr>
2023-09-22 09:58:48 +0200

    Gdesign

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
-function loadPage(pageName) {
-  fetch(pageName)
-    .then(response => response.text())
-    .then(html => {
-      document.getElementById('content').innerHTML = html;
-      closeNav();
-    })
-    .catch(error => {
-      console.error('Erreur de chargement de la page :', error);
-    });
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  loadPage('./html/main.html');
-});
 var sidenav = document.getElementById("mySidenav");
 var openBtn = document.getElementById("openBtn");
 var closeBtn = document.getElementById("closeBtn");
@@ -30,10 +15,20 @@ function closeNav() {
   sidenav.classList.add("close-nav");
 }
 
-
-
-
-
-
-
-
+function page(Name){
+  if (Name === "main"){
+    window.location.href = "../html/main.html";
+  }
+  else if (Name === "cqnf"){
+    window.location.href = "../html/cqnf.html";
+  }
+  else if (Name === "nt"){
+    window.location.href = "../html/nt.html";
+  }
+  else if (Name === "presentation"){
+    window.location.href = "../html/presentation.html";
+  }
+  else if (Name === "nc"){
+    window.location.href = "../html/nc.html";
+  }
+}
---
+++ js/main.js
@@ -0,0 +1,44 @@
+let aff = 0;
+
+function updateImage() {
+    const imageElement = document.getElementById("imageElement"); // Obtenez l'élément ici
+
+    const imageSources1 = "../public/carousel-maine/com-one.png";
+    const imageSources2 = "../public/carousel-maine/com-two.png";
+    const imageSources3 = "../public/carousel-maine/com-tree.png";
+    const imageSources4 = "../public/carousel-maine/com-four.png";
+    const imageSources5 = "../public/carousel-maine/com-five.png";
+
+    if (aff === 0) {
+        imageElement.src = imageSources1;
+    } else if (aff === 1) {
+        imageElement.src = imageSources2;
+    } else if (aff === 2) {
+        imageElement.src = imageSources3;
+    } else if (aff === 3) {
+        imageElement.src = imageSources4;
+    } else if (aff === 4) {
+        imageElement.src = imageSources5;
+    }
+
+    console.log("aff:", aff);
+    console.log("imageElement.src:", imageElement.src);
+}
+
+
+function next() {
+    aff = aff + 1;
+    if (aff > 4) {
+        aff = 0; // Loop back to the first image
+    }
+    updateImage();
+}
+
+function prev() {
+    aff = aff - 1;
+    if (aff < 0) {
+        aff = 4; // Loop back to the last image
+    }
+    updateImage();
+}
+updateImage();
