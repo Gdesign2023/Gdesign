02a300622c4820b7ed85b7c5de2b6d0f6ed29792
lucas pasnon <lucas.pasnon@outlook.fr>
2023-09-23 09:59:27 +0200

    Gdesign

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 var sidenav = document.getElementById("mySidenav");
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3");
+var button4 = document.getElementById("button4");
+var button5 = document.getElementById("button5");
 var openBtn = document.getElementById("openBtn");
 var closeBtn = document.getElementById("closeBtn");
 
@@ -8,11 +13,21 @@ closeBtn.onclick = closeNav;
 function openNav() {
   sidenav.classList.add("active");
   sidenav.classList.remove("close-nav");
+  button1.style.borderTop="2px solid #fff";
+  button2.style.borderTop="2px solid #fff";
+  button3.style.borderTop="2px solid #fff";
+  button4.style.borderTop="2px solid #fff";
+  button5.style.borderTop="2px solid #fff";
 }
 
 function closeNav() {
   sidenav.classList.remove("active");
   sidenav.classList.add("close-nav");
+  button1.style.borderTop="none";
+  button2.style.borderTop="none";
+  button3.style.borderTop="none";
+  button4.style.borderTop="none";
+  button5.style.borderTop="none";
 }
 
 function page(Name){
