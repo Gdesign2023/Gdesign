bfd65117aedf65941699d026d04cd7e32fbc3aa5
lucas pasnon <lucas.pasnon@outlook.fr>
2023-09-25 14:03:40 +0200

    Gdesign

--- js/video.js
+++ js/video.js
@@ -2,3 +2,11 @@ const video = document.getElementById("myVideo");
 video.addEventListener("ended", function () {
   window.location.href = "./html/main.html";
 });
+const screenWidth = window.screen.width;
+const tailleVideo = document.getElementById("myVideo");
+console.log(`Screen width: ${screenWidth}`);
+if (screenWidth < 768) {
+  tailleVideo.classList.add("taille-tel");
+} else {
+  tailleVideo.classList.remove("taille-tel");
+}
